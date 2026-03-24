a285088802735d5c5992e8a38190a491f69628e5
Angelo Eustaquio <angelob.eustaquio@gmail.com>
2026-03-25 00:05:12 +0400

    A

--- Public/script.js
+++ Public/script.js
@@ -81,3 +81,15 @@ function initUpdates() {
     document.body.classList.remove("no-scroll");
   });
 }
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "w0xhog9upd");
